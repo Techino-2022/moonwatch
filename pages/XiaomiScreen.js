1a7bd3af55057b9aded83f119740c5cb3d256417
mosfazli <mosfazli@gmail.com>
2023-04-20 22:59:50 +0330

    add models for xiaomi brand

--- pages/XiaomiScreen.js
+++ pages/XiaomiScreen.js
@@ -47,31 +47,31 @@ const XiaomiScreen = props => {
             }}>
               <Box>
                 <AspectRatio w="50%" position="absolute" right="0" top="2">
-                  <Image resizeMode="contain" opacity="10" source={{
-                    uri: "https://raw.githubusercontent.com/Techino-2022/moonwatch/main/assets/images/huawei_logo.png"
-                  }} alt="huawei logo"
+                  <Image resizeMode="contain" opacity="90" source={{
+                    uri: "https://raw.githubusercontent.com/Techino-2022/moonwatch/main/assets/images/xiaomi_models/MI_band4.png"
+                  }} alt="Xiaomi mi band 4"
                   />
                 </AspectRatio>
               </Box>
               <Stack p="4" space={3}>
                 <Stack space={2}>
                   <Heading size="md" ml="-1">
-                    Huawei smartwatches
+                    Xiaomi MI band 4
                   </Heading>
-                  <Text fontSize="xs" _light={{
+                  <Text fontSize="sm" _light={{
                     color: "emerald.500"
                   }} _dark={{
                     color: "emerald.500"
                   }} fontWeight="500" ml="-0.5" mt="-1">
-                    include: Huawei/Honor band 5, Huwaei/Honor band 6, Huwaei/Honor band 7
+                    Total Watchfaces: 32
                   </Text>
                 </Stack>
-                <HStack alignItems="center" space={4} justifyContent="space-between">
+                <HStack alignItems="center" space={4} fontSize="xs" justifyContent="space-between">
                   <HStack alignItems="center">
                     <Text color="coolGray.600" _dark={{
                       color: "warmGray.200"
                     }} fontWeight="400">
-                      Last update: 6 mins ago
+                      Last update: 1 mins ago
                     </Text>
                   </HStack>
                 </HStack>
@@ -85,44 +85,81 @@ const XiaomiScreen = props => {
               borderWidth: 0
             }} _light={{
               backgroundColor: "gray.50"
-            }}
-            >
-              <Pressable onPress={() =>
-              navigate('XiaomiModel')
-            }>
-                <Box>
-                  <AspectRatio w="50%" position="absolute" right="0" top="2">
-                    <Image resizeMode="contain" opacity="10" source={{
-                      uri: "https://raw.githubusercontent.com/Techino-2022/moonwatch/main/assets/images/xiaomi_logo.png"
-                    }} alt="huawei logo"
-                    />
-                  </AspectRatio>
-                </Box>
-                <Stack p="4" space={3}>
-                  <Stack space={2}>
-                    <Heading size="md" ml="-1">
-                      Xiaomi smartwatches
-                    </Heading>
-                    <Text fontSize="xs" _light={{
-                      color: "emerald.500"
-                    }} _dark={{
-                      color: "emerald.500"
-                    }} fontWeight="500" ml="-0.5" mt="-1">
-                      include: Mi band 5, Mi band 6, Mi band 7
-                    </Text>
-                  </Stack>
-                  <HStack alignItems="center" space={4} justifyContent="space-between">
-                    <HStack alignItems="center">
-                      <Text color="coolGray.600" _dark={{
-                        color: "warmGray.200"
-                      }} fontWeight="400">
-                        Last update: 1 hour ago
-                      </Text>
-                    </HStack>
-                  </HStack>
+            }}>
+              <Box>
+                <AspectRatio w="50%" position="absolute" right="0" top="2">
+                  <Image resizeMode="contain" opacity="90" source={{
+                    uri: "https://raw.githubusercontent.com/Techino-2022/moonwatch/main/assets/images/xiaomi_models/Mi_Band5.png"
+                  }} alt="Xiaomi mi band 5"
+                  />
+                </AspectRatio>
+              </Box>
+              <Stack p="4" space={3}>
+                <Stack space={2}>
+                  <Heading size="md" ml="-1">
+                    Xiaomi MI band 5
+                  </Heading>
+                  <Text fontSize="sm" _light={{
+                    color: "emerald.500"
+                  }} _dark={{
+                    color: "emerald.500"
+                  }} fontWeight="500" ml="-0.5" mt="-1">
+                    Total Watchfaces: 30
+                  </Text>
                 </Stack>
-              </Pressable>
-            </Box>
+                <HStack alignItems="center" space={4} fontSize="xs" justifyContent="space-between">
+                  <HStack alignItems="center">
+                    <Text color="coolGray.600" _dark={{
+                      color: "warmGray.200"
+                    }} fontWeight="400">
+                      Last update: 11 mins ago
+                    </Text>
+                  </HStack>
+                </HStack>
+              </Stack>
+            </Box >
+
+            <Box minW="80" maxH="80" minHeight="140" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700"
+            }} _web={{
+              shadow: 2,
+              borderWidth: 0
+            }} _light={{
+              backgroundColor: "gray.50"
+            }}>
+              <Box>
+                <AspectRatio w="50%" position="absolute" right="0" top="2">
+                  <Image resizeMode="contain" opacity="90" source={{
+                    uri: "https://raw.githubusercontent.com/Techino-2022/moonwatch/main/assets/images/xiaomi_models/Mi_Band6.png"
+                  }} alt="Xiaomi mi band 4"
+                  />
+                </AspectRatio>
+              </Box>
+              <Stack p="4" space={3}>
+                <Stack space={2}>
+                  <Heading size="md" ml="-1">
+                    Xiaomi MI band 6
+                  </Heading>
+                  <Text fontSize="sm" _light={{
+                    color: "emerald.500"
+                  }} _dark={{
+                    color: "emerald.500"
+                  }} fontWeight="500" ml="-0.5" mt="-1">
+                    Total Watchfaces: 3
+                  </Text>
+                </Stack>
+                <HStack alignItems="center" space={4} fontSize="xs" justifyContent="space-between">
+                  <HStack alignItems="center">
+                    <Text color="coolGray.600" _dark={{
+                      color: "warmGray.200"
+                    }} fontWeight="400">
+                      Last update: 8 mins ago
+                    </Text>
+                  </HStack>
+                </HStack>
+              </Stack>
+            </Box >
           </VStack>
         </VStack>
       </Center>
